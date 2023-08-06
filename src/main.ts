04516b9849c511d2ecfbfee43b241f23fed2aadf
DenverBrian <denver.talamayan@gmail.com>
2023-08-07 06:02:47 +0800

    add button to  div with id of app

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { Navbar } from "./components/Navbar";
 import "./style.css";
+import {sampleButton} from  "./denver";
+
 
 const app = document.getElementById("app");
 app?.appendChild(Navbar);
+app?.appendChild(sampleButton);
