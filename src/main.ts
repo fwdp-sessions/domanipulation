97ad5f4a200f2dc1e3273b00dc274c8bc7625928
jellyfish <tjdc@myyahoo.com>
2023-08-07 06:07:08 +0800

    fix output of jellyfish/index.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { Navbar } from "./components/Navbar";
+import { jellyButton } from "./jellyfish"
 import "./style.css";
 
 const app = document.getElementById("app");
 app?.appendChild(Navbar);
+app?.appendChild(jellyButton);
