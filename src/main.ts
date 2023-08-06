ee7035a2773c72382cf29320db43a9c7c27e962e
toney010319 <toney010319@gmail.com>
2023-08-07 06:00:27 +0800

    Added a Button

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { Navbar } from "./components/Navbar";
+import { Btn1 } from "./daydreamer";
 import "./style.css";
 
 const app = document.getElementById("app");
 app?.appendChild(Navbar);
+app?.appendChild(Btn1)
