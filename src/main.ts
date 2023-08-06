2c990144f402ea067930f8ff4187ac063ed66710
forrest <joshalvarez.dev@gmail.com>
2023-08-07 06:15:21 +0800

    Add button to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { Navbar } from "./components/Navbar";
 import "./style.css";
+import { Btn1 } from "./matchu";
 
 const app = document.getElementById("app");
 app?.appendChild(Navbar);
+app?.appendChild(Btn1);
