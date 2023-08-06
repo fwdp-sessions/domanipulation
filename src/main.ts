811a9bb62fec6c3c115fb361f6e4506e92c55584
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-08-07 06:00:26 +0800

    Added a button

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { Navbar } from "./components/Navbar";
 import "./style.css";
+import { myButton } from "./denvie";
 
 const app = document.getElementById("app");
 app?.appendChild(Navbar);
+app?.appendChild(myButton);
