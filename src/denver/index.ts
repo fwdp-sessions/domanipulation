aeb5b23e87b2bc2a0648d1e9182620af8ba32b1d
DenverBrian <denver.talamayan@gmail.com>
2023-08-08 06:14:46 +0800

    add button counter and button toggle vite logo

--- src/denver/index.ts
+++ src/denver/index.ts
@@ -5,7 +5,40 @@ export const Page = document.createElement("div"); // Do not touch this line
 //----------------------------------------
 // Code goes here
 
-Page.innerHTML = "<h1>Matchu.</h1>";
+if (Page) {
+  //create element
+  const counterButton = document.createElement("button");
+  const toggleLogoButton = document.createElement("button");
+  const logo = document.createElement("img");
+
+  //add to parent div
+  Page.appendChild(counterButton);
+  Page.appendChild(toggleLogoButton);
+
+  //set element text content
+  toggleLogoButton.textContent = `Toggle Vite Logo`;
+  counterButton.textContent = `clicked 69 times`;
+
+  //set element image content
+  logo.src = `./public/vite.svg`;
+
+  let clicks: number = 0;
+  counterButton.addEventListener("click", () => {
+    clicks++;
+    counterButton.textContent = `clicked ${clicks} times`;
+  });
+
+  let logoVisibility = false;
+  toggleLogoButton.addEventListener("click", () => {
+    if (logoVisibility === false) {
+      Page.appendChild(logo);
+      logoVisibility = true;
+    } else {
+      Page.removeChild(logo);
+      logoVisibility = false;
+    }
+  });
+}
 
 // Code should end here
 //--------------------------------------------
