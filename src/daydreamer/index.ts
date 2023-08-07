e8b6392ba8bb07ec2e56e2bc3d518b43b2c87a9f
toney010319 <toney010319@gmail.com>
2023-08-08 03:31:00 +0800

    Adding 2 buttons, Adding Form with Input password and submit button more over im doing the task

--- src/daydreamer/index.ts
+++ src/daydreamer/index.ts
@@ -1,3 +1,53 @@
 export const Btn1 = document.createElement("button")
 
-Btn1.innerHTML=("d4ydr34mer")
+Btn1.textContent=("Counter")
+Btn1.style.margin="10px"
+let counter = 0;
+Btn1.addEventListener("click", () => {
+    counter++
+    Btn1.textContent=`clicked ${counter} times`
+})
+
+export const logo = document.createElement("div")
+
+logo.innerHTML=
+                `<img src="src/daydreamer/assets/logo.png" 
+                style="width: 17%; 
+                position:absolute;
+                top: 0%;
+                left: 41%;
+                display:block ">`
+
+export const logoBtn = document.createElement("button")
+logoBtn.textContent = "Toggle Vite Logo"
+logoBtn.addEventListener('click', () => {
+    if (logo.style.display === "none") {
+        logo.style.display = "block";
+        
+      } else {
+        logo.style.display = "none";
+      }
+})
+
+export const form = document.createElement("form")
+form.style.margin="10px"
+form.style.display="flex"
+form.style.flexDirection="column"
+const PasswordInput = document.createElement("input")
+PasswordInput.type="password"
+PasswordInput.style.height ="35px"
+const submit = document.createElement("button")
+submit.innerText="Submit"
+form.appendChild(PasswordInput)
+form.appendChild(submit)
+
+export const passwordType = document.createElement("button")
+ 
+passwordType.innerText="Show Password"
+passwordType.addEventListener('click', () => {
+    if(PasswordInput.type ==="password"){
+        PasswordInput.type="show"
+    }
+    else {PasswordInput.type ="password"}
+    
+})
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,18 @@ import "./style.css";
 import { Navbar } from "./components/Navbar";
 import { Btn1 as matchu } from "./matchu";
 import { jellyButton } from "./jellyfish"
-import { Btn1 } from "./daydreamer";
+import { Btn1, form, logo, logoBtn, passwordType } from "./daydreamer";
 import { myButton } from "./denvie";
 import {sampleButton} from  "./denver";
 
 const app = document.getElementById("app");
 app?.appendChild(Navbar);
-app?.appendChild(jellyButton);
-app?.appendChild(myButton);
-app?.appendChild(sampleButton);
-app?.appendChild(matchu)
+// app?.appendChild(jellyButton);
+// app?.appendChild(myButton);
+// app?.appendChild(sampleButton);
+// app?.appendChild(matchu)
+app?.appendChild(logo)
 app?.appendChild(Btn1)
+app?.appendChild(logoBtn)
+app?.appendChild(form)
+app?.appendChild(passwordType)
