a0490b6e84141219c665b24bfe2b9fa4e2bb4cb1
directormac <markasena@gmail.com>
2023-08-09 06:18:48 +0800

    Fix daydreamers conflict

--- src/daydreamer/index.ts
+++ src/daydreamer/index.ts
@@ -5,43 +5,23 @@ Page.innerHTML = '<h1>Daydreamer.</h1>';
 //----------------------------------------
 // Code goes here
 
-<<<<<<< HEAD
-const CounterBtn = document.createElement('button');
+const counterBtn = document.createElement('button');
 
-CounterBtn.textContent = 'Counter';
+counterBtn.textContent = 'Counter';
 
 let counter = 0;
-CounterBtn.addEventListener('click', () => {
+counterBtn.addEventListener('click', () => {
   counter++;
   if (counter <= 1) {
-    CounterBtn.textContent = `clicked ${counter} time`;
+    counterBtn.textContent = `clicked ${counter} time`;
   } else {
-    CounterBtn.textContent = `clicked ${counter} times`;
+    counterBtn.textContent = `clicked ${counter} times`;
   }
 });
 
-const logo = document.createElement('div');
+const logoForVite = document.createElement('div');
 
-logo.innerHTML = `<img src="public/vite.svg" 
-=======
-const counterBtn = document.createElement("button")
-
-counterBtn.textContent=("Counter")
-
-let counter = 0;
-counterBtn.addEventListener("click", () => {
-    counter++
-    if (counter <= 1){
-        counterBtn.textContent=`clicked ${counter} time`}
-    else {counterBtn.textContent=`clicked ${counter} times`}
-   
-})
-
- const logoForVite = document.createElement("div")
-
-logoForVite.innerHTML=
-                `<img src="public/vite.svg" 
->>>>>>> 37aa163 (Change name variable from logo to logoForVite)
+logoForVite.innerHTML = `<img src="public/vite.svg" 
                 style="width: 10%; 
                 position:absolute;
                 top: 10%;
@@ -51,73 +31,38 @@ logoForVite.innerHTML=
 const logoBtn = document.createElement('button');
 logoBtn.textContent = 'Toggle Vite Logo';
 logoBtn.addEventListener('click', () => {
-<<<<<<< HEAD
-  if (logo.style.display === 'none') {
-    logo.style.display = 'block';
+  if (logoForVite.style.display === 'none') {
+    logoForVite.style.display = 'block';
   } else {
-    logo.style.display = 'none';
+    logoForVite.style.display = 'none';
   }
 });
-=======
-    if (logoForVite.style.display === "none") {
-        logoForVite.style.display = "block";
-        
-      } else {
-        logoForVite.style.display = "none";
-      }
-})
->>>>>>> dcb7987 (Change variable naming convention into camelCase and more specific, Change the password input type from show to text)
 
 const form = document.createElement('form');
 
-const PasswordInput = document.createElement('input');
-PasswordInput.type = 'password';
-PasswordInput.style.height = '35px';
+const passwordInput = document.createElement('input');
+passwordInput.type = 'password';
+passwordInput.style.height = '35px';
 const submit = document.createElement('button');
 submit.innerText = 'Submit';
-form.appendChild(PasswordInput);
+form.appendChild(passwordInput);
 form.appendChild(submit);
 
-<<<<<<< HEAD
 const passwordType = document.createElement('button');
-=======
-const passwordInput = document.createElement("input")
-passwordInput.type="password"
-passwordInput.style.height ="35px"
-const submit = document.createElement("button")
-submit.innerText="Submit"
-form.appendChild(passwordInput)
-form.appendChild(submit)
->>>>>>> dcb7987 (Change variable naming convention into camelCase and more specific, Change the password input type from show to text)
 
 passwordType.innerText = 'Show Password';
 passwordType.addEventListener('click', () => {
-<<<<<<< HEAD
-  if (PasswordInput.type === 'password') {
-    PasswordInput.type = 'show';
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
   } else {
-    PasswordInput.type = 'password';
+    passwordInput.type = 'password';
   }
 });
 
-Page.appendChild(Btn1);
-Page.appendChild(logo);
+Page.appendChild(counterBtn);
+Page.appendChild(logoForVite);
 Page.appendChild(logoBtn);
 Page.appendChild(form);
 Page.appendChild(passwordType);
-=======
-    if(passwordInput.type ==="password"){
-        passwordInput.type="text"
-    }
-    else {passwordInput.type ="password"}
-    
-})
-
-Page.appendChild(counterBtn)
-Page.appendChild(logoForVite)
-Page.appendChild(logoBtn)
-Page.appendChild(form)
-Page.appendChild(passwordType)
->>>>>>> dcb7987 (Change variable naming convention into camelCase and more specific, Change the password input type from show to text)
 // Code should end here
 //--------------------------------------------
