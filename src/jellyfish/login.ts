1f9eb456b2a36fcddccd46b10e05422cba64cdc3
jellyfish <tjdc@myyahoo.com>
2023-08-11 04:05:31 +0800

    Write if-else for show password function

--- src/jellyfish/login.ts
+++ src/jellyfish/login.ts
@@ -10,6 +10,8 @@ loginShowPassword.type = "checkbox"
 let loginPasswordArea = document.createElement("div")
 loginPasswordArea.appendChild(loginPasswordBox)
 loginPasswordArea.appendChild(loginShowPassword)
+if (loginShowPassword.checked) loginPasswordBox.type = "text"
+else loginPasswordBox.type = "password"
 
 export let output = document.createElement("div")
 output.appendChild(loginMessage)
