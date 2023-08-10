a49d42684ee6cf976ae93b509738f6a4450cc249
jellyfish <tjdc@myyahoo.com>
2023-08-11 03:59:20 +0800

    Add login message

--- src/jellyfish/login.ts
+++ src/jellyfish/login.ts
@@ -1,5 +1,6 @@
-let test = document.createElement("button")
-test.innerText = "ASD"
+let loginMessage = document.createElement("h1")
+loginMessage.innerText = "Login Here"
+
 
 export let output = document.createElement("div")
-output.appendChild(test)
+output.appendChild(loginMessage)
