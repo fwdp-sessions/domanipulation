a7791cf725e5d4f91674b2018ae1f791a715bfb9
jellyfish <tjdc@myyahoo.com>
2023-08-11 04:03:01 +0800

    Add login password and show password textbox

--- src/jellyfish/login.ts
+++ src/jellyfish/login.ts
@@ -1,6 +1,16 @@
 let loginMessage = document.createElement("h1")
 loginMessage.innerText = "Login Here"
 
+let loginPasswordBox = document.createElement("input")
+loginPasswordBox.type = "password"
+
+let loginShowPassword = document.createElement("input")
+loginShowPassword.type = "checkbox"
+
+let loginPasswordArea = document.createElement("div")
+loginPasswordArea.appendChild(loginPasswordBox)
+loginPasswordArea.appendChild(loginShowPassword)
 
 export let output = document.createElement("div")
 output.appendChild(loginMessage)
+output.appendChild(loginPasswordArea)
