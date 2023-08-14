c7327e3dd8e89c8a70a56b7caccd7f33bc38c46f
DenverBrian <denver.talamayan@gmail.com>
2023-08-15 01:11:21 +0800

    add login form set to local storage currrent user

--- src/denver/loginPage.ts
+++ src/denver/loginPage.ts
@@ -11,10 +11,24 @@ if (loginForm) {
   loginForm.appendChild(passwordLogin);
   loginForm.appendChild(loginButton);
   loginForm.appendChild(anotherRegisterLink);
+  let isLoggedIn = false;
 
- 
- 
-  
-
- 
+  loginForm.addEventListener("submit", (e) => {
+    const Users = localStorage.getItem("Users");
+    const parseUsers = JSON.parse(Users ?? "[]");
+    const loginFormInputs = new FormData(e.target as HTMLFormElement);
+    const email = loginFormInputs.get("emailLogin");
+    const password = loginFormInputs.get("passwordLogin");
+    for (let i = 0; i < parseUsers.length; i++) {
+      if (
+        parseUsers[i].email === email &&
+        parseUsers[i].password === password
+      ) {
+        isLoggedIn = true;
+        const currentUser = JSON.stringify(parseUsers[i]);
+        localStorage.setItem("User", currentUser);
+        break;
+      }
+    }
+  });
 }
